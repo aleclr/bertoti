a25a6c7e988efd7f1e1c5151ce4c336b9af82ab5
Alec Lima Rondel <alecrondel@hotmail.com>
2023-05-19 19:59:56 -0300

    Correção diagrama de classes e testes JUnit

--- Engenharia de Software/hospital/src/test/java/hospital/AppTest.java
+++ Engenharia de Software/hospital/src/test/java/hospital/AppTest.java
@@ -1,5 +1,6 @@
 package hospital;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -7,14 +8,27 @@ import org.junit.Test;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
+
+    Enfermidade covid = new Enfermidade("Febre, Dor de cabeça, Coriza", "Otorrino");
+    Paciente alec = new Paciente("Alec", 25, covid);
+
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    };
+
+    @Test
+    public void testePaciente() {
+        assertTrue("Success", alec.getNome().equals("Alec"));
+    };
+
+    @Test
+    public void testePacienteEnfermidade() {
+        assertEquals("Otorrino", alec.getEnfermidade().getDepartamento());
+    };
+
 }
